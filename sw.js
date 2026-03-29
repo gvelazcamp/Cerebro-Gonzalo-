5240b47dd94048d2720dc97ab9c53700b1c2c5fa
gvelazcamp <97753660+gvelazcamp@users.noreply.github.com>
2026-03-28 22:03:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-// Service Worker Chino Aventura - Ad Blocker v8
-var CACHE_NAME = 'chino-aventura-v8-no-ads';
+// Service Worker Chino Aventura - Anti-Ad Brutal v9
+var CACHE_NAME = 'chino-aventura-v9-no-ads-brutal';
 var urlsToCache = [
   '/Cerebro-Gonzalo-/chino_aventura.html',
   '/Cerebro-Gonzalo-/manifest.json'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('SW Install v8 - Ad Blocker');
+  console.log('SW Install v9 - Anti-Ad Brutal');
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('SW Activate v8 - Cleaning old caches');
+  console.log('SW Activate v9 - Cleaning old caches');
   event.waitUntil(
     caches.keys().then(function(names) {
       return Promise.all(
